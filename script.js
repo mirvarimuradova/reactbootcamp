df62eb471259c232ca41ed682b75b39718871a6a
mirvarimuradova <mirvarimuradova7@gmail.com>
2024-02-07 21:04:20 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Task 1
+document.getElementById('colorButton').addEventListener('click', function(){
+document.getElementById('greetingButton').style.color =("green");
+})
+//Task 2 
+document.getElementById('textButton').addEventListener('click', function(){
+    document.getElementById('greetingButton').innerText = ('message was changed');
+})
